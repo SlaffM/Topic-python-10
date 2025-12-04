d097da3d68a70187e2fd3bca0990545f24692e6c
Vyacheslav <slavkamm@gmail.com>
2025-12-04 15:35:36 +0300

    Update case_2.py

--- case_2.py
+++ case_2.py
@@ -1,5 +1,10 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import math
+from datetime import datetime as dt
+
+PAYMENT_PERIOD = 183
+END_PAYMENT_DAY = dt.fromisoformat(f'2023-03-01')
 
 def generate_report(sheet1,sheet2,sheet3):
   scope = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
@@ -7,23 +12,17 @@ def generate_report(sheet1,sheet2,sheet3):
   creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
 
   client = gspread.authorize(creds) 
-
+  
   sheet1 = client.open('Installments').worksheet(sheet1)
   sheet1_data = sheet1.get_all_records()
   sheet2 = client.open('Installments').worksheet(sheet2)
   sheet2_data = sheet2.get_all_records()
   sheet3 = client.open('Installments').worksheet(sheet3)
   sheet3_data = sheet3.get_all_records()
+  
   return sheet1_data, sheet2_data, sheet3_data 
 
 sheet1_data, sheet2_data, sheet3_data = generate_report("Лист1","Лист2","Лист3")
-
-import math
-from datetime import datetime as dt
-
-PAYMENT_PERIOD = 183
-END_PAYMENT_DAY = dt.fromisoformat(f'2023-03-01')
-
 
 def calc_dolg(student_rec): 
   expected_date = dt.strptime(student_rec['expected_payment_date'], "%d.%m.%Y")
@@ -40,7 +39,6 @@ def calc_dolg(student_rec):
   calculated_dolg = student_rec['one-time_payment'] * cnt_periods
 
   return min(calculated_dolg, student_rec['left_to_pay'])
-
 
 students_installments= [st for st in sheet1_data if st['installment']=='Y']
 students_data = []
